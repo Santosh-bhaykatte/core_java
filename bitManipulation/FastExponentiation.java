282b32726552f73a71f782eec77cbb3fa11018cb
Santosh Bhaykatte <bhaykatte2020@gmail.com>
2025-10-08 20:20:55 +0530

    Basic programs using bit manipulation

---
+++ bitManipulation/FastExponentiation.java
@@ -0,0 +1,18 @@
+package core_Java.bitManipulation;
+
+public class FastExponentiation {
+    static int fastExpo(int a, int n) {
+        int res = 1;
+        while (n != 0) {
+            if ((n & 1) != 0) {     //LSB
+                res = res * a;
+            }
+            a = a * a;
+            n = n>>1;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        System.out.println(fastExpo(3, 5));
+    }
+}
---
+++ bitManipulation/SwapNumbers.java
@@ -0,0 +1,14 @@
+package core_Java.bitManipulation;
+
+public class SwapNumbers {
+    static void swap(int a, int b) {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("a: "+ a);
+        System.out.println("b: "+ b);
+    }
+    public static void main(String[] args) {
+        swap(3, 5);
+    }
+}
