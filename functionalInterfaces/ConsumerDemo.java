eebd7723a27812bbae85bb4c628fe2867ee6be7c
Santosh Bhaykatte <bhaykatte2020@gmail.com>
2025-11-13 23:54:30 +0530

    Consumer<T> functional interface

---
+++ functionalInterfaces/ConsumerDemo.java
@@ -0,0 +1,56 @@
+/*
+
+public interface Consumer<T> {
+    void accept(T t);
+}
+
+* */
+
+package core_Java.functionalInterfaces;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        Consumer<String> printer = (s) -> System.out.println("Msg: "+ s);
+        printer.accept("Hello World");
+
+        Consumer<Integer> doubler = (n) -> System.out.println("Double: "+ (n*2));
+//        doubler.accept(10);
+
+        Consumer<Integer> increment = (i) -> System.out.println("Increment: "+ (i+1));
+//        increment.accept(5);
+
+        doubler.andThen(increment).accept(20);
+
+        //Consumer<T> for custom object
+        Consumer<User> userConsumer = user -> System.out.println("Name: "+ user.name);
+        userConsumer.accept(new User("John"));
+
+        //use of Consumer<T> behind the scenes
+        List<String> list = Arrays.asList("Santo", "Tony", "Mony");
+
+        System.out.println("Names:");
+        list.forEach(System.out::println);
+
+        /*
+        Internal Working:
+
+        1. System.out::println      -Method reference
+
+        2. Consumer<String> c = (s) -> System.out.println(s);       //Consumer Implementation
+
+        3. c.accept(listElement)
+         */
+    }
+}
