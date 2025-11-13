dd85be28399f3ff18d32960bd47b3cb7ad77e95e
Santosh Bhaykatte <bhaykatte2020@gmail.com>
2025-11-14 00:22:39 +0530

    Supplier<T> functional interface

---
+++ functionalInterfaces/SupplierDemo.java
@@ -0,0 +1,37 @@
+/*
+
+public interface Supplier<T> {
+    T get();
+}
+
+* */
+
+package core_Java.functionalInterfaces;
+
+import java.util.function.Supplier;
+
+class Student {
+    String name;
+
+    Student() {
+        this.name = "tony";
+        System.out.println("Student created!");
+    }
+}
+
+public class SupplierDemo {
+    public static void main(String[] args) {
+        Supplier<String> greet = () -> "Hello Supplier!";
+        System.out.println(greet.get());
+
+        Supplier<Integer> random = () -> (int)(Math.random()*100);
+        System.out.println("Random: "+ random.get());
+
+        //Supplier<T> for custom object
+//        Supplier<Student> studentSupplier = () -> new Student("Santo");
+
+        Supplier<Student> studentSupplier = Student::new;   //Constructor reference
+
+        Student student = studentSupplier.get();
+    }
+}
